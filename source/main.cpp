2c6f4e2701feec44781405e31ba54c2088a28196
Bali Ádám <baliadam04@gmail.com>
2024-08-30 10:27:54 +0200

    window creation

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,58 @@
 #include "main.h"
 
+constexpr GLuint SCR_WIDTH = 1280;
+constexpr GLuint SCR_HEIGHT = 720;
+
 int main()
 {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "VoxelExplorer :)", NULL, NULL);
+
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initalize GLAD!" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetFramebufferSizeCallback(window, main::framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window))
+	{
+
+		main::processInput(window);
+
+		glClearColor(0.4f, 0.6f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void main::framebuffer_size_callback(GLFWwindow* window, GLint width, GLint height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void main::processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
 }
--- source/main.h
+++ source/main.h
@@ -1,9 +1,21 @@
 #pragma once
 
-
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <stb/stb_image.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
 
 class main
 {
+public:
+	static void framebuffer_size_callback(GLFWwindow* window, GLint width, GLint height);
+	static void processInput(GLFWwindow* window);
 
 };
-
