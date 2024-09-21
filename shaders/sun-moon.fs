ba1c7e516c2500bdc7c884b2ab6f02967c72dcd9
Bali Ádám <baliadam04@gmail.com>
2024-09-21 12:52:20 +0200

    made the sun and the moon blurry using gaussian blur in the fragment shader for more realistic look

--- shaders/sun-moon.fs
+++ shaders/sun-moon.fs
@@ -5,9 +5,26 @@ in vec2 TexCoords;
 
 uniform sampler2D sunTexture;
 
+// Blur intensity
+const float blurRadius = 0.004; 
+const int samples = 10;
+
 void main() {
-    vec4 texColor = texture(sunTexture, TexCoords);
-    if(texColor.a < 0.001)
+    vec4 texColor = vec4(0.0);
+    vec2 texOffset = vec2(blurRadius);
+
+    // Apply Gaussian blur
+    for(int x = -samples; x <= samples; ++x) {
+        for(int y = -samples; y <= samples; ++y) {
+            vec2 offset = vec2(float(x), float(y)) * texOffset;
+            texColor += texture(sunTexture, TexCoords + offset);
+        }
+    }
+
+    texColor /= float((samples * 2 + 1) * (samples * 2 + 1)); // Normalize the color
+
+    if (texColor.a < 0.001)
         discard;
+
     FragColor = texColor;
 }
